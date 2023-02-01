6b72a9994f5c3671d4ec78ec3a173cb716bea24f
“Ilya_M” <balu126@yandex.ru>
2023-02-01 20:55:34 +0300

    Add telegram bot

---
+++ Homework_9/my_telegram_bot.py
@@ -0,0 +1,15 @@
+# Простой телеграм бот
+
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('6118427346:AAH3wlekyXlCZDRRAFwd0wyIdv_*********')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я 1.02. на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
