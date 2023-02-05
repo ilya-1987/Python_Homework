a43094625bac09b9fdbf371f3c863f15617188a7
“Ilya_M” <balu126@yandex.ru>
2023-02-05 23:00:26 +0300

    Add telegram bot search for wikepedia

---
+++ Homework_10/bot.py
@@ -0,0 +1,44 @@
+import telebot, wikipedia, re
+
+bot = telebot.TeleBot('6118427346:AAH3wlekyXlCZDRRAFwd0wyIdv_FwbxHq8I')
+
+wikipedia.set_lang("ru")
+
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        
+        wikitext=ny.content[:2000]
+        
+        wikimas=wikitext.split('.')
+        
+        wikimas = wikimas[:-1]
+        
+        wikitext2 = ''
+        
+        for x in wikimas:
+            if not('==' in x):
+                    
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        
+        return wikitext2
+    
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Телеграм бот ищущий на Wikipedia')
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+
+bot.polling(none_stop=True, interval=0)
